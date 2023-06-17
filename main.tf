0940200ed638ad83e7f373eac4ae40bd3ff63faf
Elena Cazan <59057287+cazanelena@users.noreply.github.com>
2023-06-17 12:20:04 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,18 @@
-    # The configuration for the `remote` backend.
-    terraform {
-      backend "remote" {
-        # The name of your Terraform Cloud organization.
-        organization = "fac28-devops-week3"
+provider "aws" {
+  region = "eu-west-2"  # Update with your desired region
+}
 
-        # The name of the Terraform Cloud workspace to store Terraform state files in.
-        workspaces {
-          name = "fac28-workspace"
-        }
-      }
-    }
+resource "aws_instance" "example" {
+  ami           = "ami-0804a2711fa3726f3"  # Update with your desired AMI ID
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "Week3-CI/CD-fac28"
+  }
 
-    # An example resource that does nothing.
-    resource "null_resource" "example" {
-      triggers = {
-        value = "A example resource that does nothing!"
-      }
-    }
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World!" > index.html
+              nohup python -m SimpleHTTPServer 80 &
+              EOF
+}
